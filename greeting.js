2ce8d22a8f750ef59a0ef1ad0530f7f2a697cd90
Jessica Cho <jessicacho2013@gmail.com>
2020-06-19 18:27:15 -0700

    greeting

--- greeting.js
+++ greeting.js
@@ -1,29 +1,43 @@
-"use strict";
-
-const form = document.querySelector(".js-form");
-input = form.querySelector("input"),
-greeting = document.querySelector(".js-greetings");
+const form = document.querySelector(".js-form"),
+  input = form.querySelector("input"),
+  greeting = document.querySelector(".js-greetings");
 
 const USER_LS = "currentUser",
-    SHOWING_CN = "showing";
+  SHOWING_CN = "showing";
 
-function painGreeting(text){
-    form.classList.remove(SH0WING_CN);
-    greetting.classList.add(SHOWING_CN);
-    greeting.innerText = `Hello ${text}`
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
+}
+
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
+
+function paintGreeting(text) {
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `Hello ${text}`;
 }
 
 function loadName() {
-const currentUser = localStorage.getItem(USER_LS);
-if(currentUser) === null){
-      //she is not
-} else {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+    askForName();
+  } else {
     paintGreeting(currentUser);
-    }
+  }
 }
 
 function init() {
-    loadName();
+  loadName();
 }
 
 init();
